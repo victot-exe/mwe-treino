6f41bdcc06dc1462ba2dec549e85271213453697
Victor <victorfarian@gmail.com>
2025-10-29 15:18:41 -0300

    initial commit

---
+++ app/_layout.tsx
@@ -0,0 +1,34 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+export default function RootLayout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar style="light" />
+      
+      <Stack>
+        <Stack.Screen
+          name="(drawer)"
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="(modals)/exercicio/novoExercicio"
+          options={{
+            presentation: "modal",
+            title: "➕ Novo Exercício",
+          }}
+        />
+
+        <Stack.Screen
+          name="(modals)/treinos/novoTreino"
+          options={{
+            presentation: "modal",
+            title: "➕ Novo Treino",
+          }}
+        />
+      </Stack>
+    </GestureHandlerRootView>
+  );
+}
